4c1e5fb25ad2ff5d5a7cc40f6137bd5fc7c4230e
Leonel <leonel_lima19@hotmail.com>
2023-03-02 08:16:18 -0300

    Format test file

--- backend/test/files.test.js
+++ backend/test/files.test.js
@@ -1,59 +1,62 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const server = require('../index');
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const server = require("../index");
 
 // Assertion style
 chai.should();
 
 chai.use(chaiHttp);
 
-describe('Files API', () => {
+describe("Files API", () => {
   /**
    * Test files list - GET route
    */
-  describe('Files list GET', () => {
-    it('Should get a list of files', (done) => {
-      chai.request(server)
-        .get('/files/list')
+  describe("Files list GET", () => {
+    it("Should get a list of files", (done) => {
+      chai
+        .request(server)
+        .get("/files/list")
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.an('object').that.include({ message: 'Ok' });
-        })
-        done();
-    })
-  })
+          res.body.should.be.an("object").that.include({ message: "Ok" });
+        });
+      done();
+    });
+  });
 
   /**
    * Test files data - GET route
    * Test files data with query param - GET route
    */
-  describe('Files data GET', () => {
-    it('Should get a list of files with their formatted lines', (done) => {
-      chai.request(server)
-        .get('/files/data')
+  describe("Files data GET", () => {
+    it("Should get a list of files with their formatted lines", (done) => {
+      chai
+        .request(server)
+        .get("/files/data")
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.an('object').that.include({ message: 'Ok' });
-          
+          res.body.should.be.an("object").that.include({ message: "Ok" });
+
           const data = res.body.data;
-          chai.expect(data).to.be.an('array').to.have.lengthOf.at.least(1);
-        })
+          chai.expect(data).to.be.an("array").to.have.lengthOf.at.least(1);
+        });
       done();
     });
-    it('Should get a file by file name with its formatted lines', (done) => {
-      chai.request(server)
-        .get('/files/data?fileName=test1.csv')
+    it("Should get a file by file name with its formatted lines", (done) => {
+      chai
+        .request(server)
+        .get("/files/data?fileName=test1.csv")
         .end((err, res) => {
           res.should.have.status(200);
-          res.body.should.be.an('object').that.include({ message: 'Ok' });
-          
+          res.body.should.be.an("object").that.include({ message: "Ok" });
+
           const data = res.body.data;
-          chai.expect(data).to.be.an('array').to.have.lengthOf(1);
-          
-          const file = data[0]
-          chai.expect(file).to.include({ file: 'test1.csv' })
-        })
+          chai.expect(data).to.be.an("array").to.have.lengthOf(1);
+
+          const file = data[0];
+          chai.expect(file).to.include({ file: "test1.csv" });
+        });
       done();
     });
-  })
-})
+  });
+});
