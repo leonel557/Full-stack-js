ecbf05008e80b8a57ad960f6c83096028271c685
Leonel <leonel_lima19@hotmail.com>
2023-03-01 21:21:17 -0300

    Added cors to server

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const app = require("express")();
-const bodyParser = require('body-parser')
+const cors = require('cors');
+const bodyParser = require("body-parser");
 
 // Parse body
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  next();
+}, cors({ maxAge: 84600, origin: process.env.NODE_ENV === "development" ? "*" : process.env.URL_APP }));
 
 // Require all routes
-app.use(require('./routes/files'));
+app.use(require("./routes/files"));
 
 // Run server
 const port = process.env.PORT || 4000;
