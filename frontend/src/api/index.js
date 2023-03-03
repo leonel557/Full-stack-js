17518d4adc582a71a2bc3ba8befa9e27b7dbe29f
Leonel <leonel_lima19@hotmail.com>
2023-03-03 08:20:59 -0300

    Api testing

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,14 +2,12 @@ import axios from "axios";
 
 const URL = "http://localhost:4000";
 
-const axiosClient = axios.create({
-  baseURL: process.env.NODE_ENV === "development" ? URL : process.env.API_URL,
-});
-
 class API {
   async get(url) {
-    const response = await axiosClient.get(url);
-    return response.data
+    const baseUrl =
+      process.env.NODE_ENV === "development" ? URL : process.env.API_URL;
+    const response = await axios.get(`${baseUrl}${url}`);
+    return response.data;
   }
 }
 
