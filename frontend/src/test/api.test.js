e210841637b271a43d45d3f28faa683a4bb9a4bc
Leonel <leonel_lima19@hotmail.com>
2023-03-03 08:37:40 -0300

    Format test file

--- frontend/src/test/api.test.js
+++ frontend/src/test/api.test.js
@@ -1,18 +1,18 @@
 import axios from "axios";
 
-import API from '../api'
+import API from "../api";
 
 jest.mock("axios");
 
-describe('API', () => {
-  it('Should get a list of files', async () => {
+describe("API", () => {
+  it("Should get a list of files", async () => {
     const files = ["file1.csv", "file2.csv", "file3.csv"];
 
-    axios.get.mockResolvedValueOnce({ data: { data: files, message: 'Ok' } });
+    axios.get.mockResolvedValueOnce({ data: { data: files, message: "Ok" } });
 
-    const { data } = await API.get('/files/list');
+    const { data } = await API.get("/files/list");
 
     expect(axios.get).toHaveBeenCalled();
     expect(data).toEqual(files);
-  })
-})
+  });
+});
